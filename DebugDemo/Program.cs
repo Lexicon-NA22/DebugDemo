d4b78812e6eeb09f6db5b01d20a5722ea11c7ab0
David Nokto <david.nokto@gmail.com>
2021-11-30 11:49:28 +0100

    Add project files.

---
+++ DebugDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace DebugDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            int a = 50;
+            int b = 50;
+
+            int result = GetSum(a, b);
+
+            Console.WriteLine(result);
+
+            int result2 = 0;
+            int[] manyNumbers = Enumerable.Range(1, 100).ToArray();
+
+            for (int i = 0; i < manyNumbers.Length; i++)
+            {
+                result2 = i;
+            }
+
+        }
+
+        private static int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
